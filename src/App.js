4bdf1ae46d6e9db6b4b146aff28ae90ef7747109
Brien Thomas <thomasbrien94@gmail.com>
2022-06-09 18:01:12 -0700

    framework and basic styling for the zoo started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [bicepSize, setBicepSize] = useState(5);
+  const [quadSize, setQuadSize] = useState(5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="App">
+      <header><h2>Muscle Zoo</h2></header>
+      <div className='muscle-competition'>
+        <div className='left'>
+          <img src='https://place-puppy.com/300x300' />
+          <button>Left Animal</button>
+        </div>
+        <div className='right'>
+          <img src='https://place-puppy.com/300x300' />
+          <button>Right Animal</button>
+        </div>
+      </div>
+      
+
+      <div>Open/Closed
+
+      </div>
+
+      <div>Animal Parade
+
+      </div>
+    </body>
   );
 }
 
